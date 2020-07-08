a0520b20b1ae089a9465e2f7fd5e0f56aade0902
jhj960918@gmail.com <jhj960918@gmail.com>
2020-07-08 19:16:22 +0900

    글 목록 출력

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs': blogs})
+
 def new(request):
     full_text = request.GET['fulltext']
 
     word_list = full_text.split()
-    
+
     word_dictionary = {}
 
     for word in word_list:
